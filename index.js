bdcd986089cf580a3f646b3d9a2d50857557a374
Sumit-337 <sumitguptadsa21@gmail.com>
2025-08-16 21:13:44 +0530

    second commit

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 require("dotenv").config();
 const express = require("express");
+const pool = require("./config/db");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send(`🚀 Server running on port ${PORT}`);
+});
+app.get("/test", (req, res) => {
+  res.send(`testing the server`);
 });
 
 app.listen(PORT, () => {
